c4533a6bd89832f1e2500ec7dca83c90a886945f
sourbiskit <ruanvictor.qa@gmail.com>
2023-02-23 20:09:16 -0300

    Add project files.

---
+++ MarketPlaceOnline2023.AlfaPeople.MyPlugins/AccountManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceOnline2023.AlfaPeople.MyPlugins
+{
+    public class AccountManager : IPlugin
+    {
+        public IOrganizationService Service { get; set; }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            Service = serviceFactory.CreateOrganizationService(context.UserId);
+        }
+    }
+}
